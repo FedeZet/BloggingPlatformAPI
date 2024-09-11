af389c27d4e1b2101d7db0e5b6a36372755068dd
FedeZet <fcoelhovazquez@gmail.com>
2024-09-11 19:46:47 -0300

    Removing BlogRepository class

--- src/main/kotlin/dev/fedezet/BloggingPlatformAPI/persistence/repository/BlogRepository.kt
+++
@@ -1,9 +0,0 @@
-package dev.fedezet.BloggingPlatformAPI.persistence.repository
-
-import dev.fedezet.BloggingPlatformAPI.persistence.entity.Blog
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface BlogRepository: CrudRepository<Blog, Long> {
-}
